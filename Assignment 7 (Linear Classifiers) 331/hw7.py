173295fc2ed624fa9c0b9c997fbc708e66c621de
evenegas3 <evenegas3@users.noreply.github.com>
2020-05-05 20:32:01 -0700

    get_data() complete

--- Assignment 7 (Linear Classifiers) 331/hw7.py
+++ Assignment 7 (Linear Classifiers) 331/hw7.py
@@ -6,17 +6,35 @@ import matplotlib.pyplot as plt
 import scipy.io as sio
 
 def get_data():
-    print('5')
+    X_correct = np.load('X_correct.npy')
+    y_correct = np.load('y_correct.npy')
+    # x = sio.loadmat('mnist-original.mat')['data']
+    # y = sio.loadmat('mnist-original.mat')['label']
 
-def get_train_and_test_sets(X, y):
-    pass
+    return X_correct, y_correct
+
+def get_train_and_test_sets(x, y):
+    """
+    x and y are arrays
+    """
+    limit = 60000
+    x_training = x[:limit][:]
+    x_testing = x[limit:]
+
+    y_training = y[:limit]
+    y_testing = y[limit:]
+    # print(x_training.shape, x_testing.shape)
+    # print(y_training.shape, y_testing.shape)
+
 
 def train_to_data(x,y,z):
     # X[:100], y[:100], 'SGD'
     pass
 
 def main():
-    print('$')
+    x, y = get_data()
+    get_train_and_test_sets(x, y)
+    # print('$')
 
 main()    
 # if __name__ == "__main__":
