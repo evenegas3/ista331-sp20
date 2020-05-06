5b719183b84d7f59b8c9cb619a33d5cfc985f4d4
evenegas3 <42683810+evenegas3@users.noreply.github.com>
2020-05-06 15:45:13 -0700

    Update

--- Assignment 6 (Trees) 331/hw6.py
+++ Assignment 6 (Trees) 331/hw6.py
@@ -65,4 +65,31 @@ def main():
     # np.random.seed(23)
     # matrix_3 = make_and_test_tree(train_X, train_y, test_X, test_y, 3)
 
-main()
+
+
+# main()
+
+
+
+def make_depth_plot(x, y, max_depth, model_type):
+    averages = np.zeros(max_depth) 
+    error = np.zeros(max_depth) 
+
+    for i in range(1, max_depth+1):
+        if model_type == 'tree':
+            model = DecisionTreeRegressor(max_depth=1)
+        if model_type == 'forest':
+            model = RandomForestRegressor(n_estimators=25, max_depth=1)
+        
+        score = cross_val_score(model, x, y, cv = 10, scoring='neg_mean_squared_error')
+        averages[i-1] = np.average(score)
+        error[i-1] = np.std(score) / np.sqrt(10)
+
+    plt.title('Neg MSE vs. depth, model_type = {}'.format(model_type))
+    plt.errorbar(np.range(1, max_depth + 1), averages, yerr=error, color='black', capsize=5)
+    plt.xlabel('max_depth')
+    plt.ylabel('Negative mean squared error')
+
+    return np.argmax(averages)+1
+
+# make_depth_plot()
