3782454248ab60f1452574b34b219c5f0815ec0a
evenegas3 <42683810+evenegas3@users.noreply.github.com>
2020-05-05 19:50:20 -0700

    All setup for hw7

---
+++ Assignment 7 (Linear Classifiers) 331/hw7.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import SGDClassifier, LogisticRegression
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+
+def get_data():
+    print('5')
+
+
+
+def main():
+    print('$')
+    
+if __name__ == "__main__":
+    main()
