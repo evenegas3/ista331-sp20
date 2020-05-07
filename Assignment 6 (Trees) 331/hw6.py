26b19698fc292348f44f64e7b546f66330033ff1
evenegas3 <evenegas3@users.noreply.github.com>
2020-05-06 17:58:42 -0700

    need main()

--- Assignment 6 (Trees) 331/hw6.py
+++ Assignment 6 (Trees) 331/hw6.py
@@ -71,44 +71,84 @@ def get_regression_X_and_y(data):
 
     return train_x, test_x, train_y, test_y
 
-def make_depth_plot(X, y, max_depth, model_type):
-    averages = np.zeros(max_depth) 
-    error = np.zeros(max_depth) 
+def make_depth_plot(X, y, n, key):
+    lis = []
+    for i in range(1, n+1):
+        if key == 'tree':
+            model = DecisionTreeRegressor(max_depth=i)
+        if key == 'forest':
+            model = RandomForestRegressor(n_estimators=25, max_depth=i)
+        score = cross_val_score(model, X, y, cv=5, scoring='neg_mean_squared_error')
+        lis.append(score)
+    
+    lis = np.array(lis)
+    meanl = lis.mean(axis=1)
+    sd = lis.std(axis=1)
 
-    for i in range(1, max_depth+1):
-        if model_type == 'tree':
-            model = DecisionTreeRegressor(max_depth=1)
-        if model_type == 'forest':
-            model = RandomForestRegressor(n_estimators=25, max_depth=1)
-        
-        score = cross_val_score(model, X, y, cv = 10, scoring='neg_mean_squared_error')
-        averages[i-1] = np.average(score)
-        error[i-1] = np.std(score) / np.sqrt(10)
+    plt.errorbar(np.arange(len(meanl)), meanl, (sd/np.sqrt(5)))
 
-    plt.title('Neg MSE vs. depth, model_type = {}'.format(model_type))
-    plt.errorbar(np.arange(1, max_depth + 1), averages, yerr=error, color='black', capsize=5)
-    plt.xlabel('max_depth')
-    plt.ylabel('Negative mean squared error')
+    return meanl.argmax()+1
 
-    return np.argmax(averages)+1
+def compare_regressors(train_X, train_y, test_X, test_y, lis):
+    # dtr, rfr = lis
+
+    tree_mse = np.sum((lis[0].predict(train_X) - train_y) ** 2) / len(train_y)
+    rfr_mse = np.sum((lis[1].predict(train_X) - train_y) ** 2) / len(train_y)
+
+    tree_r2 = 1-tree_mse/np.var(train_y)
+    forest_r2 = 1-rfr_mse/np.var(train_y)
+
+    tree_rmse = np.sqrt(np.sum((lis[0].predict(test_X) - test_y) ** 2) / len(test_y))
+    rfr_rmse = np.sqrt(np.sum((lis[1].predict(test_X) - test_y) ** 2) / len(test_y))
+
+
+    # dtr, rfr = lis
+    # tree_mse = np.sum(dtr.predict(train_X-train_y)**2) / len(train_y)
+    # rfr_mse = np.sum(rfr.predict(train_X-train_y)**2) / len(train_y)
+
+    # tree_r2 = 1-tree_mse/np.var(train_y)
+    # forest_r2 = 1-rfr_mse/np.var(train_y)
+
+    # tree_rmse = np.sum(dtr.predict(test_X-test_y)**2) / len(test_X)
+    # rfr_rmse = np.sum(rfr.predict(test_X-test_y)**2) / len(test_y)
+
+    # print("""-----------------------------------\
+    #     Model type:   {}\
+    #         Depth:      {}\
+    #             R^2:        {}
+    # Testing RMSE:{}
+    # -----------------------------------
+    # Model type:   {}
+    # Depth:      {}
+    # R^2:        {}
+    # Testing RMSE:{}
+    
+    # """.format('DecisionTreeRegressor', dtr.max_depth,np.round(tree_r2, 4), np.round(tree_mse,4),
+    # 'RandomForestRegressor', rfr.max_depth,np.round(forest_r2, 4), np.round(rfr_mse,4)
+
+    # ))
+
+    print('-----------------------------------')
+    print('Model type:  ','DecisionTreeRegressor')
+    print('Depth:       ',lis[0].max_depth)
+    print('R^2:         ',np.round(tree_r2,4))
+    print('Testing RMSE:',np.round(tree_rmse,4))
+
+    print('-----------------------------------')
+    print('Model type:  ','RandomForestRegressor')
+    print('Depth:       ',lis[1].max_depth)
+    print('R^2:         ',np.round(forest_r2, 4))
+    print('Testing RMSE:',np.round(rfr_rmse,4))
+
+
+
+    return None
+
+
 
 def main():
     pass
     train_df, test_df = get_classification_frames()
-
-    # train_X = pd.read_pickle('train_X_correct.pkl')
-    # train_y = pd.read_pickle('train_y_correct.pkl')
-    # test_X = pd.read_pickle('test_X_correct.pkl')
-    # test_y = pd.read_pickle('test_y_correct.pkl')
-    
-    # matrix_1_correct = pd.read_pickle('matrix_1_correct.pkl')
-    # matrix_3_correct = pd.read_pickle('matrix_3_correct.pkl')
-
-    # np.random.seed(23)
-
-    # make_and_test_tree(train_X, train_y, test_X, test_y, 1)
-    # # np.random.seed(23)
-    # # matrix_3 = make_and_test_tree(train_X, train_y, test_X, test_y, 3)
 
 
 
